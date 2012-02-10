99f5d5d0eaba891a485faf4b650b4f52df4228db
Aleksandr Priymak <aleksandr.priymak@gmail.com>
2012-02-10 23:16:20 +0400

    Fixed the ray test

--- tests/rayTest.cc
+++ tests/rayTest.cc
@@ -1,24 +1,20 @@
-#ifndef RAYTEST_
-#define RAYTEST_
+#include "catch.hpp"
 
-#include "../ray.h"
-#include <gtest/gtest.h>
+#include <maxwell/ray.h>
 
-TEST(Ray, GetPoint)
+TEST_CASE("Ray/GetPoint", "")
 {
 	Ray r(Vector3(), Vector3(1,0,0));
 	
 	for (int i = 0; i < 100; i++)
 	{
-		EXPECT_EQ(Vector3(i,0,0), r.GetPoint(i));
+	        REQUIRE(Vector3(i,0,0) == r.GetPoint(i));
 	}
 	
 	Ray r2(Vector3(), Vector3(0,2,3));
 	
 	for (int i = 0; i < 100; i++)
 	{
-		EXPECT_EQ(Vector3(0,2*i,3*i), r2.GetPoint(i));
+		REQUIRE(Vector3(0,2*i,3*i) == r2.GetPoint(i));
 	}
 }
-
-#endif /*RAYTEST_*/
