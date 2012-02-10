7cab16a12e46f224b562e04266562eeaaa110aea
Aleksandr Priymak <aleksandr.priymak@gmail.com>
2012-02-10 23:10:27 +0400

    Fixed the intersection test

--- tests/intersectionTest.cc
+++ tests/intersectionTest.cc
@@ -1,13 +1,11 @@
-#ifndef INTERSECTIONTEST_H_
-#define INTERSECTIONTEST_H_
+#include "catch.hpp"
 
-#include "../primitives/plane.h"
-#include "../primitives/ball.h"
-#include "../ray.h"
-#include <gtest/gtest.h>
+#include <maxwell/primitives/plane.h>
+#include <maxwell/primitives/ball.h>
+#include <maxwell/ray.h>
 
 //TODO: Add some tests
-TEST(Intersection, Ball)
+TEST_CASE("Intersection/Ball", "")
 {
 	Material m(Color(), 0.0);
 	
@@ -15,41 +13,41 @@ TEST(Intersection, Ball)
 	Ball s2(Vector3(4,-3,0), 1, m, false, "");
 	
 	double result = 0;
-	EXPECT_EQ(1, s1.GetIntersection(Ray(Vector3(), Vector3(1,1,0).Normalize()), &result));
-	EXPECT_FLOAT_EQ(3*sqrt(2) - 1, result);
+	REQUIRE(1 == s1.GetIntersection(Ray(Vector3(), Vector3(1,1,0).Normalize()), &result));
+	REQUIRE(Approx(3*sqrt(2) - 1) == result);
 
 	result = 0;	
-	EXPECT_EQ(1, s2.GetIntersection(Ray(Vector3(), Vector3(4,-3,0).Normalize()), &result));
-	EXPECT_FLOAT_EQ(4, result);
+	REQUIRE(1 == s2.GetIntersection(Ray(Vector3(), Vector3(4,-3,0).Normalize()), &result));
+	REQUIRE(4 == result);
 	
 	for (int i = 0; i < 100; i++)
 	{
 		Ray r(Vector3(3, 3, 0), Vector3(rand(), rand(), rand()).Normalize());
 		Ray r2(Vector3(4, -3, 0), Vector3(rand(), rand(), rand()).Normalize());
 		
-		EXPECT_EQ(-1, s1.GetIntersection(r, &result));
-		EXPECT_EQ(-1, s2.GetIntersection(r2, &result)); 
+		REQUIRE(-1 == s1.GetIntersection(r, &result));
+		REQUIRE(-1 == s2.GetIntersection(r2, &result));
 	}
 	
-	EXPECT_EQ(0, s1.GetIntersection(Ray(Vector3(), Vector3(1,0,0)), &result));
-	EXPECT_EQ(0, s2.GetIntersection(Ray(Vector3(), Vector3(1,0,0)), &result));
+	REQUIRE(0 == s1.GetIntersection(Ray(Vector3(), Vector3(1,0,0)), &result));
+	REQUIRE(0 == s2.GetIntersection(Ray(Vector3(), Vector3(1,0,0)), &result));
 }
 
 //TODO: Add some tests
-TEST(Intersection, Plane)
+TEST_CASE("Intersection/Plane", "")
 {
 	Material m(Color(), 0.0);
 	Plane p1(Vector3(0,0,1), Vector3(0,0,1), m, false, "");
 	
 	double result = 0;
-	EXPECT_EQ(0, p1.GetIntersection(Ray(Vector3(), Vector3(1,0,0)), &result));
+	REQUIRE(0 == p1.GetIntersection(Ray(Vector3(), Vector3(1,0,0)), &result));
 	
 	result = 0;
-	EXPECT_EQ(0, p1.GetIntersection(Ray(Vector3(), Vector3(0,1,0)), &result));
+	REQUIRE(0 == p1.GetIntersection(Ray(Vector3(), Vector3(0,1,0)), &result));
 	
 	result = 0;
-	EXPECT_EQ(1, p1.GetIntersection(Ray(Vector3(), Vector3(0,0,1)), &result));
-	EXPECT_FLOAT_EQ(1, result);
+	REQUIRE(1 == p1.GetIntersection(Ray(Vector3(), Vector3(0,0,1)), &result));
+	REQUIRE(1.0 == Approx(result).epsilon(0.0001));
 	
 	for (int i = 0; i < 100; i++)
 	{
@@ -60,8 +58,8 @@ TEST(Intersection, Plane)
 
 		if (r1 != 0)
 		{
-			EXPECT_EQ(1, r1);
-			EXPECT_LE(1, result);
+			REQUIRE(1 == r1);
+			REQUIRE(1 <= result);
 		}
 
 		result = 0;
@@ -69,12 +67,11 @@ TEST(Intersection, Plane)
 
 		if (r2 != 0)
 		{
-			EXPECT_EQ(1, r2);
-			EXPECT_LE(1, result);
+			REQUIRE(1 == r2);
+			REQUIRE(1 <= result);
 		}
 		
-		EXPECT_EQ(0 ,(r1*r2));
+		REQUIRE(0 == (r1*r2));
 	}
 }
 
-#endif /*INTERSECTIONTEST_H_*/
